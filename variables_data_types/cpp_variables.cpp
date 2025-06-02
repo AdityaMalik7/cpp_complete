2db51927a6622fc7fe8a888374986595e72758f5
finepro <adityamalik2833@gmail.com>
2025-06-02 14:06:10 +0530

    variables

---
+++ variables_data_types/cpp_variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 5;
+  float f = 1.2;
+  double d = 1.23;
+  char ch = 'a';
+  string strs = "Hello!";
+  bool b = true;
+
+  cout << a << endl;
+  cout << f << endl;
+  cout << d << endl;
+  cout << ch << endl;
+  cout << strs << endl;
+  cout << b << endl;
+  return 0;
+}
